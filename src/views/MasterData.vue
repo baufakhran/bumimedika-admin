a7b152dad8227a5c8d1eaa05b07e72001885be8a
Bau fakhran <andi.fakhran@sekolahmu.co.id>
2022-12-19 09:11:50 +0700

    base sidebar

---
+++ src/views/MasterData.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <SideBar />
+    <v-card elevation="1">sakjk</v-card>
+  </div>
+</template>
+
+<script>
+import SideBar from '@/components/SideBar'
+
+export default {
+  name: 'HomeView',
+
+  components: {
+    SideBar
+  }
+}
+</script>
